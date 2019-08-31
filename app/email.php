5fc507c26a37941bb88b98428bda5ceadfc55756
Milos Krstic <milos@heroplugins.com>
2019-08-31 12:46:52 +0200

    * Switched to soft delete for deleting directory entries

--- app/email.php
+++ app/email.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Email extends Model
 {
     //
     public $table = 'email_directory';
+    use SoftDeletes;
 }
