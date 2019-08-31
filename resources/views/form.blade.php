fdaef497f7cd77d73c6fad2d97d25f60c64233a1
Milos Krstic <milos@heroplugins.com>
2019-08-31 13:11:12 +0200

    * Set autocomplete to off for input fields * Increased font size for input fields

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -17,15 +17,15 @@
 
             <div class="form_left_column">
                 <label for="name">Name*</label>
-                <input type="text" name="name" class="{{ $errors->has('name') ? 'field_error' : '' }}" value="{{ old('name') }}" required>
+                <input type="text" name="name" class="{{ $errors->has('name') ? 'field_error' : '' }}" value="{{ old('name') }}" required autocomplete="off">
             </div>
             <div class="form_right_column">
                 <label for="surname">Surname</label>
-                <input type="text" name="surname" value="{{ old('surname') }}">
+                <input type="text" name="surname" value="{{ old('surname') }}" autocomplete="off">
             </div>
             <div class="form_single_column">
                 <label for="email">Email*</label>
-                <input type="text" name="email" class="{{ $errors->has('email') ? 'field_error' : '' }}" value="{{ old('email') }}" required>
+                <input type="text" name="email" class="{{ $errors->has('email') ? 'field_error' : '' }}" value="{{ old('email') }}" required autocomplete="off">
             </div>
 
             @if($errors->any())
