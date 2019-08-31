858e7adbeb1c0b750e1370e9c7629201f45f783e
Milos Krstic <milos@heroplugins.com>
2019-08-31 12:51:04 +0200

    * Removed soft delete functionality as it confuses inserting new entries because of unique email requirement

--- app/email.php
+++ app/email.php
@@ -3,11 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Email extends Model
 {
     //
     public $table = 'email_directory';
-    use SoftDeletes;
 }
